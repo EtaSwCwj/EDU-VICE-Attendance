d5bbc3e9dd3eee3072aec2d2ebe2f144362589c2
EtaSwCwj <cwj@etaelec.com>
2025-10-08 00:54:19 +0900

    런 테스크 추가

---
+++ scripts/scrcpy_dev.ps1
@@ -0,0 +1,8 @@
+# --- scrcpy_dev.ps1 ---
+$links = "$env:LOCALAPPDATA\Microsoft\WinGet\Links"
+if (Test-Path $links) { $env:Path += ";$links" }
+
+$scr = "$env:LOCALAPPDATA\Microsoft\WinGet\Packages\Genymobile.scrcpy_Microsoft.Winget.Source_8wekyb3d8bbwe\scrcpy-win64-v3.3.3"
+if (Test-Path $scr) { $env:Path += ";$scr" }
+
+scrcpy --max-fps 120 --video-bit-rate 16M --turn-screen-off --stay-awake --always-on-top
